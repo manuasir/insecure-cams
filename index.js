6a6215b9e9cec20b3ac4bc77ef83621a3c687e40
manuasir <manu.asi2@gmail.com>
2017-11-19 22:41:53 +0100

    improving use of iceberg

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 
-(async () => {
-	const Crawler = require('node-iceberg')
-	const _ = require('lodash')
-	const fs = require('fs')
-	// Root URL
-	 const url = 'http://www.insecam.org/en/byrating/'
-     let conf = { iteratorElement: { url: url, iterator: '?page=', maxPage: 500 }, selector: { element: 'img', attrib: 'src' } }
-      const crawl = new Crawler(url)
-        
-	try{
-		await crawl.start(conf)
-        const wholeTree = crawl.treeToObject()
-		
-		let payloads = _.flattenDeep(_.map(wholeTree.children, 'selector'))
-		// Write the JSON to disc
-		fs.writeFile('allCameras.json', JSON.stringify(payloads,null,4), 'utf8',() => {
-		  return 0
-		});
-		
-	} catch(err) {
-		throw err
-	}
-})();
+const Crawler = require('node-iceberg')
+const _ = require('lodash')
+const fs = require('fs')
+// Root URL
+const url = 'http://www.insecam.org/en/byrating/'
+let conf = { iteratorElement: { url: url, iterator: '?page=', maxPage: 500 }, selector: { element: 'img', attrib: 'src' } }
+const crawl = new Crawler(url)
+
+crawl.start(conf).then((wholeTree) => {
+  let payloads = _.flattenDeep(_.map(wholeTree.children, 'selector'))
+// Write the JSON to disc
+  fs.writeFile('allCameras.json', JSON.stringify(payloads,null,4), 'utf8',() => {  return 0 })
+}).catch((err) => { console.error("un error ",err)})
+
+
+
